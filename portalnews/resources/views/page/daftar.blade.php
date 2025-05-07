149307106aa3b93512c42773e35d2ea25a6b3f5c
rezkyputra <rezky.putra46@gmail.com>
2025-05-07 17:25:10 +0800

    latihan templating

--- portalnews/resources/views/page/daftar.blade.php
+++ portalnews/resources/views/page/daftar.blade.php
@@ -1,22 +1,17 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-<body>
-    <h1>Halaman Daftar</h1>
-    <a href="/">Kembali</a>
-    <form action="/dashboard" method="POST">
-        @csrf
-        <label>Full Name</label> <br>
-        <input type="text" name="fname"> <br> <br>
-        <label>Address</label> <br>
-        <textarea name="address" cols="30" rows="10"></textarea> <br> <br>
+@extends('layouts.master')
+@section('title')
+Halaman Daftar
+@endsection
+@section('content')
+    
+<a href="/">Kembali</a>
+<form action="/dashboard" method="POST">
+    @csrf
+    <label>Full Name</label> <br>
+    <input type="text" name="fname"> <br> <br>
+    <label>Address</label> <br>
+    <textarea name="address" cols="30" rows="10"></textarea> <br> <br>
 
-        <input type="submit" value="Sign up">
-    </form>
-</body>
-</html>
+    <input type="submit" value="Sign up">
+</form>
+@endsection
